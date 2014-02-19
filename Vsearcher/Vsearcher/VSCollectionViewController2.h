23d4b6bca8d8003e25a67ee2ba984ede64696a37
Élèves <eleves@Mac-Mini-2-1.local>
2014-02-19 16:23:08 +0100

    Mise à jour du storyboard, création des 2 classes CollectionView

---
+++ Vsearcher/Vsearcher/VSCollectionViewController2.h
@@ -0,0 +1,13 @@
+//
+//  VSCollectionViewController2.h
+//  Vsearcher
+//
+//  Created by Élèves on 19/02/2014.
+//  Copyright (c) 2014 EMSE ES. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSCollectionViewController2 : UICollectionViewController
+
+@end
